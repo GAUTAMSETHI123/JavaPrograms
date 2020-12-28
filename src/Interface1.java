4abbdc8959eda39b3a016daacf3b8794ab8b29a7
gautam <gautam.sethi_cs19@gla.ac.in>
2020-12-28 16:06:35 +0530

    use of interface in Tester.java

---
+++ src/Interface1.java
@@ -0,0 +1,24 @@
+public interface Interface1 {
+    public static final int FEES = 2000;
+    public abstract void display();
+}
+
+class Tester1 implements Interface1{
+    public void display(){
+        System.out.println("i am available now");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("the fees is "+ FEES);
+        Tester1 tester = new Tester1();
+        tester.display();
+
+        Interface1 inter = new Tester1();  // upcasting
+        inter.display();
+
+      // CTE becoz object of interface cant be created..  Interface1 inter2 = new Interface1() {
+      //  inter2.display();
+
+       // }
+    }
+}
