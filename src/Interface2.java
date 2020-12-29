971d6f79c3fb256ffc39f72420cc5f4a8d75c081
gautam <gautam.sethi_cs19@gla.ac.in>
2020-12-29 15:40:41 +0530

    implementing multiple interfaces using a class in Interface2.java

---
+++ src/Interface2.java
@@ -0,0 +1,18 @@
+public interface Interface2 {
+    void print();
+}
+interface Interface3{
+    void print();
+}
+
+class Inter implements Interface2, Interface3{
+    public void print(){
+        System.out.println("hello");
+    }
+
+
+    public static void main(String[] args) {
+        Inter inter = new Inter();
+        inter.print();
+    }
+}
