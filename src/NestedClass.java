0be5a8cb990f8f065bb9bd51e5151e4470ea926e
gautam <gautam.sethi_cs19@gla.ac.in>
2020-12-31 16:01:37 +0530

    made a nested inner class in NestedClass.java

---
+++ src/NestedClass.java
@@ -0,0 +1,23 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Gautam Sethi (GAUTAMSETHI123)
+ *   Date: 31-12-2020
+ *   Time: 15:59
+ *   File: NestedClass.java
+ */
+
+public class NestedClass {
+    int x = 10;
+    static int y =13;
+
+    static class NestedClass2{
+        void display(){
+            System.out.println(y);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(y);
+        NestedClass2 nest = new NestedClass2();
+        nest.display();
+    }
+}
